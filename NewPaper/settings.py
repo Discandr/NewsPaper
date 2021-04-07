67fad23fd075eaa1b46e7f1bd2b4117f39a6cc3c
Discandr <78156611+Discandr@users.noreply.github.com>
2021-04-07 09:08:31 +0200

    Add files via upload

--- NewPaper/settings.py
+++ NewPaper/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'django.contrib.sites',
     'django.contrib.flatpages',
     'news',
@@ -126,3 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
